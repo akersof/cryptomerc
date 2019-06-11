8ad8f5374e3e478edf86b13dd2d206ade858f375
akersof <akersof@gmail.com>
2019-06-11 12:52:25 +0200

    generic useContract hooks

--- src/contract.js
+++ src/contract.js
@@ -1,7 +1,38 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useReducer, useContext, useEffect, useState} from 'react';
 import {DappContext} from "./dapp";
 import {MetaMaskContext} from "./metamask";
 
+const contractReducer = (state, action) => {
+    switch(action.type) {
+        case "INIT":
+            return {...state, status: "init"}
+        case 'LOADING':
+            return {...state, status: "loading"};
+        case 'ERROR':
+            return {...state, status: "error"};
+        case 'SUCCESS':
+            return {...state, status: "success", result: action.result};
+        default:
+            return {...state, status: "error"};
+    }
+};
+
+
+export const useContract = (funcName, params) => {
+    //TODO check for funcName present in ABI
+    //TODO check if funcNAme is const or not, to check if we need a signer, or go with a signer call only?
+    const dappContext = useContext(DappContext);
+    const metaMaskContext = useContext(MetaMaskContext);
+    const [state, dispatch] = useReducer(contractReducer, {status: "loading"});
+    useEffect(() => {
+        if(dappContext.ready)
+            (async () => {
+                const result = await dappContext.contract.functions[funcName](...params);
+                dispatch({type:"SUCCESS", result: result});
+            })();}, [metaMaskContext.address, metaMaskContext.network, dappContext.ready]);
+    return [state];
+};
+/*
 export const useIsRegistered = () =>  {
     const dappContext = useContext(DappContext);
     const metaMaskContext = useContext(MetaMaskContext);
@@ -11,6 +42,7 @@ export const useIsRegistered = () =>  {
         if(dappContext.ready)
         (async () => {
             setLoading(true);
+            console.log(dappContext.contract);
             const registered = await dappContext.contract.isRegistered(metaMaskContext.address);
             setRegistered(registered);
             setLoading(false);
@@ -18,3 +50,4 @@ export const useIsRegistered = () =>  {
     return [isRegistered, loading];
 };
 
+*/
--- src/cryptomerc.js
+++ src/cryptomerc.js
@@ -2,16 +2,19 @@
 import React, {useContext} from "react";
 import {MetaMaskContext} from "./metamask";
 import {DappContext} from "./dapp";
-import {useIsRegistered} from './contract';
+import {useContract} from './contract';
 
 
 export const CryptoMerc = () => {
     const metaMaskContext = useContext(MetaMaskContext);
     const dappContext = useContext(DappContext);
-    const [isRegistered, isLoading] = useIsRegistered();
-    if(isLoading) return <p>LOADING....</p>
-    else if(isRegistered)
-        return <p>you are register bro</p>
-    else
-        return <p>you are not registered</p>
+    const [response] = useContract("isRegistered", [metaMaskContext.address]);
+    if(response.status === "loading") return <p>LOADING....</p>
+    if(response.status === "success"){
+        if(response.result === true)
+            return <p>you are register bro</p>
+        else if(response.result === false)
+            return <p>you are not registered</p>
+    }
+    else return <p>????</p>;
 };
